1855092f49c9994a431e35a1dffce61475d12d0e
Ashikthapriya <ashiktha.priya8@gmail.com>
2022-08-02 13:03:14 +0530

    springbootgitrepo

---
+++ src/main/java/com/services/springgitrepo/springbootgitrepo/SpringbootgitrepoApplication.java
@@ -0,0 +1,13 @@
+package com.services.springgitrepo.springbootgitrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootgitrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootgitrepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/services/springgitrepo/springbootgitrepo/SpringbootgitrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.springgitrepo.springbootgitrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootgitrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
